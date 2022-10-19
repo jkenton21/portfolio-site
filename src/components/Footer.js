4ab81b1ef109fe610baf173c11af23de8037ca51
jkenton21 <jkenton21@gmail.com>
2022-10-19 14:45:19 -0500

    Footer content edited to add links and images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,33 @@
 import React, { useEffect } from "react";
+import atLogo from "../assets/at.jpg";
+import phoneLogo from "../assets/cellphone.jpg";
+import headshotPhoto from "../assets/headshot.jpg";
 
 const Footer = () => {
     return (
         <div className="FooterContainer">
-          Jared Kenton
+          Contact Me
           <div className="socials">
-            <div>LinkedIn</div>
-            <div>GitHub</div>
-            <div>email</div>
-            <div>phone number</div>
+            Feel free to get in touch if you want to learn more!
+            <div>
+              <i class="devicon-linkedin-plain"> </i>
+              <a href="https://www.linkedin.com/in/jared-kenton-989a54a8/">Jared Kenton</a>
+            </div>
+            <div>
+              <i class="devicon-github-original-wordmark"> </i>
+              <a href="https://github.com/jkenton21">jkenton21</a>
+            </div>
+            <div>
+              <img src={atLogo} alt="email logo" />
+              jkenton21@live.com
+            </div>
+            <div>
+              <img src={phoneLogo} alt="phone logo" />
+              (913) 972-0127
+            </div>
+          </div>
+          <div className="headshot">
+            <img className="profilePic" src={headshotPhoto} alt="personal headshot" height="200px" />
           </div>
         </div>
     );
