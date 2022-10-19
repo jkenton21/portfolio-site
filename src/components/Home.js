97cb67c2b351cb927d9c2799b378c3b6dbbf1c5e
jkenton21 <jkenton21@gmail.com>
2022-10-19 15:35:14 -0500

    links and images added for the Home and Projects sections with links to deployed projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ const Home = () => {
             <div className="About">After getting my degree in Electrical Engineering,
              I went on to become a Project Manager for an electrical controls company for 5 years.
              However, during this time I learned that my passion was for programming and started
-             my journey towards becoming a software developer! I have learned a lot really enjoyed
-             the projects that I have gotten to work on. I look forward to continuing my learning and 
-             getting a job in tech!</div>
+             my journey towards becoming a software developer! I have learned a lot and really enjoyed
+             the projects that I have gotten to work on. I look forward to getting a job in tech and
+             continuing to learn even more along the way!</div>
         </div>
     );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,41 @@
 import React from "react";
+import calculatorScreenshot from "../assets/calculator.jpg";
+import todoScreenshot from "../assets/ToDo.jpg";
+import weatherappScreenshot from "../assets/Weather.jpg";
+import memorygameScreenshot from "../assets/MemoryGame.jpg";
 
 const Projects = () => {
     return (
         <div className="ProjectsContainer">
             <div className="Header">Projects</div> 
+            <div className="HeaderDesc">These are some of the projects that
+              I believe showcase what I have learned and what I am capable of.
+              I look forward to learning even more and adding to this portfolio.</div>
             <div className="ProjectsCardContainer">
-                <div className="ProjectCard">Project 1</div>
-                <div className="ProjectCard">Project 2</div>
-                <div className="ProjectCard">Project 3</div>
-                <div className="ProjectCard">Project 4</div>
-                <div className="ProjectCard">Project 5</div>
-                <div className="ProjectCard">Project 6</div>
+                <div className="ProjectCard">
+                    <img src={calculatorScreenshot} alt="project screenshot" height="75px"/>
+                    <a href="https://jkenton21.github.io/Calculator/">Basic Calculator App</a>
+                </div>
+                <div className="ProjectCard">
+                    <img src={todoScreenshot} alt="project screenshot" height="75px"/>
+                    <a href="https://jkenton21.github.io/todo-list/">To-Do List</a>
+                </div>
+                <div className="ProjectCard">
+                    <img src={weatherappScreenshot} alt="project screenshot" height="75px"/>
+                    <a href="https://jkenton21.github.io/weather-app/">Weather App</a>
+                </div>
+                <div className="ProjectCard">
+                    <img src="" alt="project screenshot" height="75px"/>
+                    <a href="">Battleship</a>
+                </div>
+                <div className="ProjectCard">
+                    <img src={memorygameScreenshot} alt="project screenshot" height="75px"/>
+                    <a href="https://jkenton21.github.io/memory-card/">Memory Card Game</a>
+                </div>
+                <div className="ProjectCard">
+                    <img src="" alt="project screenshot" height="75px"/>
+                    <a href="">Shopping Cart</a>
+                </div>
             </div>
         </div>
     );
