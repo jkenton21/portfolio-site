b52e6dc30dafebcf7d02b43f785e80286340e9d2
jkenton21 <jkenton21@gmail.com>
2022-10-19 11:03:24 -0500

    App.js file edited, components folder created to house the modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import Home from "./components/Home";
+import { Switch, Route } from "react-router-dom";
 import './App.css';
 
+const fakeRequest = () =>
+  new Promise((resolve) => setTimeout(() => resolve(), 2000));
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fakeRequest().then(() => {
+      const loader = document.querySelector(".loader-container");
+      if (loader) {
+        loader.remove();
+        setLoading(!loading);
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="Header">
+        <h1>Jared Kenton</h1>
+        <img></img>
       </header>
+      <div className="projects">
+        <Home />
+      </div>
+      <footer className="Footer">
+        Jared Kenton
+      </footer>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import Footer from "/footer";
+
+const Home = () => {
+    return (
+        <>
+        <div className="Projects"></div>
+        </>
+    );
+};
+
+export default Home;
