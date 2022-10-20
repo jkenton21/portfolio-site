67fcd2c51e4a693501d9d25b53735e33d0052379
jkenton21 <jkenton21@gmail.com>
2022-10-20 09:42:38 -0500

    More styling added to make the site look prettier and add some basic animations

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,28 +13,46 @@ const Projects = () => {
               I look forward to learning even more and adding to this portfolio.</div>
             <div className="ProjectsCardContainer">
                 <div className="ProjectCard">
-                    <img src={calculatorScreenshot} alt="project screenshot" height="75px"/>
-                    <a href="https://jkenton21.github.io/Calculator/">Basic Calculator App</a>
+                    <img src={calculatorScreenshot} alt="project screenshot" height="90%"/>
+                    <div className="ProjectLinks">
+                        <a href="https://github.com/jkenton21/Calculator"><i class="devicon-github-original colored"></i></a>
+                        <a href="https://jkenton21.github.io/Calculator/">Basic Calculator App</a>
+                    </div>
                 </div>
                 <div className="ProjectCard">
-                    <img src={todoScreenshot} alt="project screenshot" height="75px"/>
-                    <a href="https://jkenton21.github.io/todo-list/">To-Do List</a>
+                    <img src={todoScreenshot} alt="project screenshot" height="90%"/>
+                    <div className="ProjectLinks">
+                        <a href="https://github.com/jkenton21/todo-list"><i class="devicon-github-original colored"></i></a>
+                        <a href="https://jkenton21.github.io/todo-list/">To-Do List</a>
+                    </div>
                 </div>
                 <div className="ProjectCard">
-                    <img src={weatherappScreenshot} alt="project screenshot" height="75px"/>
-                    <a href="https://jkenton21.github.io/weather-app/">Weather App</a>
+                    <img src={weatherappScreenshot} alt="project screenshot" height="90%"/>
+                    <div className="ProjectLinks">
+                        <a href="https://github.com/jkenton21/weather-app"><i class="devicon-github-original colored"></i></a>
+                        <a href="https://jkenton21.github.io/weather-app/">Weather App</a>
+                    </div>
                 </div>
                 <div className="ProjectCard">
-                    <img src="" alt="project screenshot" height="75px"/>
-                    <a href="">Battleship</a>
+                    <img src="" alt="project screenshot" height="90%"/>
+                    <div className="ProjectLinks">
+                        <a href="https://github.com/jkenton21/battleship"><i class="devicon-github-original colored"></i></a>
+                        <a href="">Battleship</a>
+                    </div>
                 </div>
                 <div className="ProjectCard">
-                    <img src={memorygameScreenshot} alt="project screenshot" height="75px"/>
-                    <a href="https://jkenton21.github.io/memory-card/">Memory Card Game</a>
+                    <img src={memorygameScreenshot} alt="project screenshot" height="90%"/>
+                    <div className="ProjectLinks">
+                        <a href="https://github.com/jkenton21/memory-card"><i class="devicon-github-original colored"></i></a>
+                        <a href="https://jkenton21.github.io/memory-card/">Memory Card Game</a>
+                    </div>
                 </div>
                 <div className="ProjectCard">
-                    <img src="" alt="project screenshot" height="75px"/>
-                    <a href="">Shopping Cart</a>
+                    <img src="" alt="project screenshot" height="90%"/>
+                    <div className="ProjectLinks">
+                        <a href="https://github.com/jkenton21/shopping-cart"><i class="devicon-github-original colored"></i></a>
+                        <a href="">Shopping Cart</a>
+                    </div>
                 </div>
             </div>
         </div>
